04f9e057f386bad1caaaded9113a1232f60cd8ea
anhensam4 <ahs2k3@gmail.com>
2024-03-17 12:10:12 +0700

    fix tb_server, add requirment.txt

--- src/main/resources/python_server/tb_server.py
+++ src/main/resources/python_server/tb_server.py
@@ -1,6 +1,5 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
-from pyeda.inter import *
 import fasttext.util
 
 class CustomHandler(SimpleHTTPRequestHandler):
@@ -10,13 +9,7 @@ class CustomHandler(SimpleHTTPRequestHandler):
         query_params = parse_qs(parsed_url.query)
 
         # Check the path to determine the route
-        if parsed_url.path == '/tb':
-            # Handle the "/tb" route
-            self.handle_truth_table(query_params)
-        elif parsed_url.path == '/simplify':
-            # Handle the "/simplify" route
-            self.handle_simplify(query_params)
-        elif parsed_url.path == '/vectorization':
+        if parsed_url.path == '/vectorization':
             self.handle_vectorization(query_params)
         else:
             # Default response for unknown routes
@@ -24,30 +17,6 @@ class CustomHandler(SimpleHTTPRequestHandler):
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write("Not Found".encode('utf-8'))
-
-    def handle_truth_table(self, query_params):
-        # Get the value of a specific parameter (e.g., 'param')
-        param_value = query_params.get('param', [''])[0]
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        # Send a response with the truth table
-        response = f"{expr2truthtable(expr(param_value))}"
-        self.wfile.write(response.encode('utf-8'))
-
-    def handle_simplify(self, query_params):
-        # Get the value of a specific parameter (e.g., 'param')
-        param_value = query_params.get('param', [''])[0]
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        # Send a response with the simplified expression
-        response = f"200"
-        self.wfile.write(response.encode('utf-8'))
         
     def handle_vectorization(self, query_params):
         # Get the value of a specific parameter (e.g., 'param')
